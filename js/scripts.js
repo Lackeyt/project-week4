1439917a9cea69eac18f89a838f93f3c6723b531
Tyson Lackey <lackeyt90@gmail.com>
2020-06-19 10:06:37 -0700

    add UI, refactor priceCalc

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,16 @@
-$(document).ready(function() {
+
   //Business Logic
   function Pizza(size, toppings) {
     this.size = size;
     this.toppings = toppings;
+  };
+
+  Pizza.prototype.toppingsPriceCalc = function(){
+    let toppingsPrice = 0
+    for (i=0; i < this.toppings.length; i++){
+      toppingsPrice +=1
+    }
+    return toppingsPrice
   }
 
   Pizza.prototype.priceCalc = function(){
@@ -10,25 +18,37 @@ $(document).ready(function() {
     switch(this.size){
       case "Small":
         price = 10
-        foreach(this.toppings){
-          price += 1
-        }
+        price += this.toppingsPriceCalc()
         return price
       case "Medium":
         price = 15
-        foreach(this.toppings){
-          price += 1
-        }
+        price += this.toppingsPriceCalc()
         return price
       case "Large":
         price = 20
-        foreach(this.toppings){
-          price += 1
-        }
+        price += this.toppingsPriceCalc()
         return price
     };
   };
 
+  function toppings(){
+
+  }
 
   //User Interface
+$(document).ready(function() {
+  $("#pizzaOptions").submit(function(){
+    event.preventDefault()
+    let pizzaSize = $("input:radio[name=size]:checked").val()
+    let pizzaToppings = []
+      $("input:checkbox[name=topping]:checked").each(function(i){
+        pizzaToppings[i] = $(this).val();
+      })
+    
+    let pizzaOrder = new Pizza(pizzaSize, pizzaToppings)
+    let price = pizzaOrder.priceCalc()
+
+    console.log(pizzaOrder)
+    console.log(price)
+  })
 })
