42b2275c174fb9fdff50f84e63cf81e7b43126f7
Tyson Lackey <lackeyt90@gmail.com>
2020-06-19 14:59:45 -0700

    add comments to JS

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,7 @@
 
   Pizza.prototype.priceCalc = function(){
     let sizePrice
-    let totalPrice = this.toppings.length
+    let totalPrice = this.toppings.length //$1 per topping
     switch(this.size){
       case "Small":
         sizePrice = 10
@@ -20,10 +20,11 @@
         break;
     };
     totalPrice += sizePrice
-    return [sizePrice, totalPrice]
+    return [sizePrice, totalPrice] //stores size and total separately for output
   };
 
   //User Interface
+  //assembles toppings array
   function toppings(){
     let pizzaToppings = []
     $("input:checkbox[name=topping]:checked").each(function(i){
@@ -32,12 +33,14 @@
     return pizzaToppings
   }
 
+  //lists toppings output on checkout screen
   function showCheckoutToppings(pizzaToppings){
     pizzaToppings.forEach(
       element => $(".toppingsChoice").append(`<li>${element}</li>`)
     )
   }
 
+  //lists toppings output on order confirmation screen
   function orderConfirmedToppings(pizzaOrder){
     let output
     if (pizzaOrder.toppings.length === 0) {
@@ -49,6 +52,7 @@
   }
   
 $(document).ready(function() {
+  //on submission of size & topping selections
   $("#pizzaOptions").submit(function(){
     event.preventDefault()
     let pizzaOrder = new Pizza($("input:radio[name=size]:checked").val(), toppings())
@@ -63,6 +67,7 @@ $(document).ready(function() {
     $("#pizzaOptions").toggle()
   })
 
+  //reverts back to submissions screen and clears checkout output
   $("#goBack").click(function(){
     event.preventDefault()
     $(".sizeChoice").empty()
@@ -73,12 +78,14 @@ $(document).ready(function() {
     $("#pizzaOptions").show()
   })
 
+  //confirms order and proceeds to order confirmation screen
   $("#order").click(function(){
     event.preventDefault()
     $("#checkout").hide()
     $("#orderConfirmed").show()
   })
 
+  //reloads the page
   $("#startOver").click(function(){
     location.reload()
   })
