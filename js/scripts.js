31ec4be9e80292095144e51575a44e0e38cbd7e4
Tyson Lackey <lackeyt90@gmail.com>
2020-06-19 09:03:15 -0700

    update price logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,34 @@
 $(document).ready(function() {
   //Business Logic
-  function Pizza() {
-    this.size = 
-    this.toppings
+  function Pizza(size, toppings) {
+    this.size = size;
+    this.toppings = toppings;
   }
 
   Pizza.prototype.priceCalc = function(){
+    let price
+    switch(this.size){
+      case "Small":
+        price = 10
+        foreach(this.toppings){
+          price += 1
+        }
+        return price
+      case "Medium":
+        price = 15
+        foreach(this.toppings){
+          price += 1
+        }
+        return price
+      case "Large":
+        price = 20
+        foreach(this.toppings){
+          price += 1
+        }
+        return price
+    };
+  };
 
-  }
 
-  
   //User Interface
 })
